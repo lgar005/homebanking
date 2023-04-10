18c61dc63af75245e26b682dbe7043ab8441fe2a
lgar005 <garla005@gmail.com>
2023-04-10 16:14:49 -0500

    front-end improvements

--- src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -24,7 +24,6 @@ public class HomebankingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomebankingApplication.class, args);
-		System.out.println("hola");
 	}
 
 	@Bean
