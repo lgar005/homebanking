dca61bcc33c128eb7de1f9cf4c8c25df45429c3c
lgar005 <garla005@gmail.com>
2023-04-13 14:58:00 -0500

    optional in clientController

--- src/main/java/com/mindhub/homebanking/controllers/ClientController.java
+++ src/main/java/com/mindhub/homebanking/controllers/ClientController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 import static java.util.stream.Collectors.toList;
 
@@ -22,7 +23,7 @@ public class ClientController {
         return clientRepository.findAll().stream().map(client -> new ClientDTO(client)).collect(toList());
     }
     @RequestMapping("/api/clients/{id}")
-    public ClientDTO getClient(@PathVariable Long id){
-        return  clientRepository.findById(id).map(client -> new ClientDTO(client)).orElse(null);
+    public Optional<ClientDTO> getClient(@PathVariable Long id){
+        return  clientRepository.findById(id).map(client -> new ClientDTO(client));
     }
 }
